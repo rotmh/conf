e99da1fa8a1dc651a6fd6e7c672da9e44b54bfc9
rotmh <rotmh@proton.me>
2025-10-08 14:57:42 +0300

    feat: bump gwfox's version

--- pkgs/gwfox/package.nix
+++ pkgs/gwfox/package.nix
@@ -6,13 +6,13 @@
 
 stdenvNoCC.mkDerivation {
   pname = "gwfox";
-  version = "2.7.19";
+  version = "2.23";
 
   src = fetchFromGitHub {
     owner = "akkva";
     repo = "gwfox";
-    rev = "2ea734ce0ee65b31bcc57a58a8956c0db9a15f2c"; # 2.7.19
-    sha256 = "sha256-p8eNbGNy/sxMBJ0GEJ3sFMCCehqMwrzs9rhFb6Vg6pY=";
+    rev = "a754dedbc66ede897e05e5e911db7604a9328d08";
+    sha256 = "sha256-2OlfCOYvdnIOs3SyGC+cqzsSOFFDKcvmdHk8EvZ7CQg=";
   };
 
   patches = [
