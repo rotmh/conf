c79f4866d13b12c2444ec13ea5f51411a3727cb5
Rotem Horesh <rotmh@proton.me>
2025-11-27 18:20:15 +0200

    Replace the pinned old stremio with a new version
    This uses an informal packaging of stremio-linux-shell.
    
    Related issues: #1

--- flake.nix
+++ flake.nix
@@ -4,13 +4,6 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
-    # qt5 has been flagged as unmaintained and insecure, so we must explicitly
-    # permit its usage to run Stremio. However, since insecure packages are not
-    # built by Hydra once marked with known vulnerabilities, we use a pinned,
-    # older nixpkgs revision from before that change. This ensures Hydra can
-    # provide prebuilt binaries, since building qt5 locally is too heavy.
-    nixpkgs-for-stremio.url = "nixpkgs/fdaf9ac0214d7dea1b3e573434cd96c0d10c9d76";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -48,6 +41,8 @@
     };
 
     helix-git.url = "github:helix-editor/helix";
+
+    nixohess.url = "gitlab:fazzi/nixohess";
   };
 
   nixConfig = {
--- modules/home-manager/stremio.nix
+++ modules/home-manager/stremio.nix
@@ -10,11 +10,6 @@ let
   ns = lib'.modulesNamespace;
 
   cfg = config.${ns}.stremio;
-
-  stremioPkgs = import inputs.nixpkgs-for-stremio {
-    inherit (pkgs.stdenv.hostPlatform) system;
-    config.allowUnfree = true;
-  };
 in
 {
   options.${ns}.stremio = {
@@ -23,10 +18,14 @@ in
 
   config = lib.mkIf cfg.enable {
     ${ns}.impermanence.directories.symlink = [
+      # Cache
       ".stremio-server"
-      ".local/share/Smart Code ltd/Stremio"
+      # Credentials (user auth) among others
+      ".local/share/.stremio-wrapped"
     ];
 
-    home.packages = [ stremioPkgs.stremio ];
+    home.packages = [
+      inputs.nixohess.packages.${pkgs.stdenv.hostPlatform.system}.stremio-linux-shell
+    ];
   };
 }
