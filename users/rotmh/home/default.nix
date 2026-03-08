d9377648b2a9c827dd0e8806f061af8c817b67ad
Rotem Horesh <rotmh@proton.me>
2026-03-08 10:54:14 +0200

    Add kakoune

--- users/rotmh/home/default.nix
+++ users/rotmh/home/default.nix
@@ -140,6 +140,31 @@ in
     };
   };
 
+  programs.kakoune = {
+    enable = true;
+    plugins = [ pkgs.kakounePlugins.kakoune-lsp ];
+    extraConfig = ''
+      eval %sh{kak-lsp}
+      lsp-enable
+
+      set-option global modelinefmt "%opt{lsp_modeline} %opt{modelinefmt}"
+
+      map global user l ':enter-user-mode lsp<ret>' -docstring 'LSP mode'
+
+      map global goto d <esc>:lsp-definition<ret> -docstring 'LSP definition'
+      map global goto r <esc>:lsp-references<ret> -docstring 'LSP references'
+      map global goto y <esc>:lsp-type-definition<ret> -docstring 'LSP type definition'
+
+      map global insert <tab> '<a-;>:try lsp-snippets-select-next-placeholders catch %{ execute-keys -with-hooks <lt>tab> }<ret>' -docstring 'Select next snippet placeholder'
+
+      map global object a '<a-semicolon>lsp-object<ret>' -docstring 'LSP any symbol'
+      map global object <a-a> '<a-semicolon>lsp-object<ret>' -docstring 'LSP any symbol'
+      map global object f '<a-semicolon>lsp-object Function Method<ret>' -docstring 'LSP function or method'
+      map global object t '<a-semicolon>lsp-object Class Interface Module Namespace Struct<ret>' -docstring 'LSP class or module'
+      map global object d '<a-semicolon>lsp-diagnostic-object error warning<ret>' -docstring 'LSP errors and warnings'
+      map global object D '<a-semicolon>lsp-diagnostic-object error<ret>' -docstring 'LSP errors'    '';
+  };
+
   programs.bat.enable = true;
 
   programs.yazi = {
